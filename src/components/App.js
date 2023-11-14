142e9fd3d16a12d26db4e7a33b2ea603ffd25bbb
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-11-13 20:35:29 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import '../styles/App.css';
+import logo from "../RadiantLightAllWhite.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div id="logo-backdrop">
+        <img src={logo} style={{opacity: "70%"}} width="700px" alt="" />
+      </div>
+      <div className='row palette'>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925825/IMG_0780_m8jyrd.jpg" alt="" />
+        </div>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925822/IMG_0703_k5vwtu.jpg" alt="" />
+        </div>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925703/IMG_0467_iv7ohc.jpg" alt="" />
+        </div>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925727/IMG_0583_amucdr.jpg" alt="" />
+        </div>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925670/IMG_0448_eimcd3.jpg" alt="" />
+        </div>
+        <div className='col-2'>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925765/IMG_0491_dx0xbk.jpg" alt="" />
+        </div>
+      </div>
+      <div className='row'>
+        <div className='container col-4'>
+          <h1 className='header-title julius-so'>Ready to shine?</h1>
+          <h3 className='header-sub archivo'>Radiant Light Hair Design</h3>
+        </div>
+        <div className='container col-4'>
+          <h1 className='header-title playfair'>Ready to shine?</h1>
+          <h3 className='header-sub raleway'>Radiant Light Hair Design</h3>
+        </div>
+        <div className='container col-4'>
+          <h1 className='header-title playfair'>Ready to shine?</h1>
+          <h3 className='header-sub source-s3'>Radiant Light Hair Design</h3>
+        </div>
+        <div className='container col-4'>
+          <h1 className='header-title julius-so'>Ready to shine?</h1>
+          <h3 className='header-sub raleway'>Radiant Light Hair Design</h3>
+        </div>
+        <div className='container col-4'>
+          <h1 className='header-title yeseva'>Ready to shine?</h1>
+          <h3 className='header-sub bebas-neue'>Radiant Light Hair Design</h3>
+        </div>
+        <div className='container col-4'>
+          <h1 className='header-title julius-so'>Ready to shine?</h1>
+          <h3 className='header-sub yeseva'>Radiant Light Hair Design</h3>
+        </div>
+      </div>
+      <div className='row photos'>
+        <div className='col-3' style={{margin: "0px", padding: "0px"}}>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925702/IMG_0489_i4l2xt.jpg" width="100%" alt="" />
+        </div>
+        <div className='col-6' style={{margin: "0px", padding: "0px"}}>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925701/IMG_0475_zurj9z.jpg" width="112.5%" alt="" />
+        </div>
+        <div className='col-3' style={{margin: "0px", padding: "0px"}}>
+          <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1699925785/IMG_0612_jngbvf.jpg" width="100%" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
