18e3e45d6d3a4524c34811d2b75de752c8d44654
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-02-14 14:17:30 -0600

    no book

--- src/components/NavSmall.js
+++ src/components/NavSmall.js
@@ -15,9 +15,6 @@ const NavSmall = () => {
           <li aria-current="page" id="services-link" className="nav-small-li raleway">
             <h3><Link to="/services">Services</Link></h3>
           </li>
-          <li aria-current="page" className="nav-small-li raleway">
-            <h3><a href="https://radiantlighthd.glossgenius.com" target="popup" onClick={() => window.open('https://radiantlighthd.glossgenius.com/','Book an Appointment with USERNAME','width=600,height=400')}>Book</a></h3>
-          </li>
           <li aria-current="page" id="shop-link" className="nav-small-li raleway">
             <h3><Link to="https://cp.salonhq.co/radiant-light-hair-design">Shop</Link></h3>
           </li>
