d0b4656f86c4a0271ea0586e6cdb7372af4b194c
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-02-22 16:39:01 -0600

    index.css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import './styles/index.css';
-// import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import App from "./components/App.js"
 import createStore from 'react-auth-kit/createStore';
