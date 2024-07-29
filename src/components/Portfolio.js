b88a7282082bf0340f4c5cb75d58e2f61c658a09
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-07-29 10:47:36 -0500

    gallery photo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,14 +11,25 @@ const Portfolio = () => {
   }, []);
 
   return (
-    <div id="portfolio">
-      <div className="row" id="wedding-row">
-        <h1 id="wedding-title">Weddings</h1>
-        <WeddingWidget />
+    <div>
+      <div className="row" id="gallery-header-row">
+        <div className="col service-image-div">
+          <div className="gallery-image" id="gallery-image-one"></div>
+        </div>
       </div>
-      <div className="row" id="colors-row">
-        <h1 id="colors-title">Colors</h1>
-        <ColorWidget />
+      <div id="gallery-hero-text">
+        <h1>Gallery</h1>
+        <h3> </h3>
+      </div>
+      <div id="portfolio">
+        <div className="row" id="wedding-row">
+          <h1 id="wedding-title">Weddings</h1>
+          <WeddingWidget />
+        </div>
+        <div className="row" id="colors-row">
+          <h1 id="colors-title">Colors</h1>
+          <ColorWidget />
+        </div>
       </div>
     </div>
   )
