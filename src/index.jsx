1109f713fd75e562ec7d6f779af825c749669bce
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-07-29 23:58:11 -0500

    migrating to vite from CRA

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from 'react-router-dom';
 import "./styles/App.css";
 import "./styles/index.css";
-import App from "./components/App.js"
+import App from "./components/App.jsx"
 import createStore from 'react-auth-kit/createStore';
 import AuthProvider from 'react-auth-kit';
 import { Route, Routes } from 'react-router';
 import AuthOutlet from '@auth-kit/react-router/AuthOutlet';
-import Event from './components/Event.js';
-import PageNotFound from './components/PageNotFound';
-import About from './components/About';
-import Contact from './components/Contact';
-import Home from './components/Home';
-import Portfolio from './components/Portfolio';
-import Services from './components/Services';
-import Admin from './components/Admin';
-import AdminPrivate from './components/AdminPrivate';
-import NavSmall from './components/NavSmall';
+import Event from './components/Event.jsx';
+import PageNotFound from './components/PageNotFound.jsx';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import Home from './components/Home.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Services from './components/Services.jsx';
+import Admin from './components/Admin.jsx';
+import AdminPrivate from './components/AdminPrivate.jsx';
+import NavSmall from './components/NavSmall.jsx';
 
 const store = createStore({
   authName:'_auth',
